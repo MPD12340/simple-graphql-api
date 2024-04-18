86ea3c05769bc412b9a715268625b27fd669ef51
Mahendra Prasad Dhamala <rijalmpd1234@gmail.com>
2024-04-18 13:41:50 +0545

    graphql first commit

---
+++ data.js
@@ -0,0 +1,5 @@
+export const users = [
+    { id: '1', name: 'John Doe', username: 'johndoe123', age: 30, nationality: 'USA' },
+    { id: '2', name: 'Jane Smith', username: 'janesmith456', age: 25, nationality: 'Canada' },
+    { id: '3', name: 'Alice Johnson', username: 'alicej', age: 35, nationality: 'UK' }
+  ];
