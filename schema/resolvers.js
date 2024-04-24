94371fa9890156773d8569835d0a0a2ca188ae6a
Mahendra Prasad Dhamala <rijalmpd1234@gmail.com>
2024-04-25 00:30:30 +0545

    added create user api

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,15 +1,26 @@
 import { users, movies } from "../data.js";
-import _ from 'lodash'
+import _ from "lodash";
 
 export const resolvers = {
-    Query: {
-        users : () => users,
-        user: (parent, args) => _.find(users, {id: args.id}),
-        movies: () => {return movies},
-        movie: (parent, args) => {
-            const title = args.title
-            console.log(args)
-            return _.find(movies, {title: title})
-        }
-    }
+  Query: {
+    users: () => users,
+    user: (parent, args) => _.find(users, { id: args.id }),
+    movies: () => {
+      return movies;
+    },
+    movie: (parent, args) => {
+      const title = args.title;
+      console.log(args);
+      return _.find(movies, { title: title });
+    },
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const lastElemID = users[users.length - 1].id;
+      user.id = lastElemID + 1;
+      users.push(user);
+      return user;
+    },
+  },
 };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -26,6 +26,17 @@ type Query {
     movies: [Movie!]!
     movie (title: String!): Movie!
 }
+
+input createUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality = BRAZIL
+}
+type Mutation {
+createUser (input: createUserInput!) :  User
+}
+
 enum Nationality {
     CANADA
     BRAZIL
